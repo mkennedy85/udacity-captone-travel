43adf29863b7f17c3ac25781b2293e31d942c287
Mike Kennedy <michael_l_kennedy@apple.com>
2021-01-17 16:45:24 -0800

    Updated

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,4 @@ import { handleSubmit } from "./js/formHandler";
 export { handleSubmit };
 
 import "./styles/base.scss";
-import "./styles/footer.scss";
 import "./styles/form.scss";
-import "./styles/header.scss";
-
